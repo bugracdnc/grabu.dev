f533cee3765999c1ae722156eeccfb08bb6089d0
bugracdnc <bugracaliskandinc@gmail.com>
2024-09-30 15:27:23 +0300

    Moved port to cmd line

--- servlet.go
+++ servlet.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"strings"
 	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
 func servePdf(w http.ResponseWriter, r *http.Request) {
@@ -19,11 +20,18 @@ func servePdf(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	port := os.Args[1:]
+
+	if len(port) == 0 || port == nil {
+		fmt.Printf("usage: CV :[port]\n\ne.g.\tCV :80, CV :443\n")
+		return
+	}
+
 	http.Handle("/", http.FileServer(http.Dir("static")))
 	http.HandleFunc("/cv", servePdf)
 
-	fmt.Println("Starting server on :443...")
-	err := http.ListenAndServe(":443", nil)
+	fmt.Printf("Starting server on %s...\n\n", port[0])
+	err := http.ListenAndServe(port[0], nil)
 	if err != nil {
 		log.Panic(err)
 	}
