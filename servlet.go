9e46ac2b24e435c4851af6d276ffbc123f3656e0
bugracdnc <bugracaliskandinc@gmail.com>
2024-09-30 10:24:01 +0300

    Added servlet for returning either html or pdf

---
+++ servlet.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func servePdf(w http.ResponseWriter, r *http.Request) {
+	locale := r.URL.Query().Get("locale")
+	cvEn := "static/pdf/cv-grabu-dev-en.pdf"
+	cvTr := "static/pdf/cv-grabu-dev-tr.pdf"
+	w.Header().Set("Content-Type", "application/pdf")
+	if strings.Contains(strings.ToLower(locale), "tr") {
+		http.ServeFile(w, r, cvTr)
+	}
+	http.ServeFile(w, r, cvEn)
+}
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("static")))
+	http.HandleFunc("/pdf", servePdf)
+
+	fmt.Println("Starting server on :80...")
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
