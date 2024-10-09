9317d93eebc6496cc2efd6de351a79186746a76c
bugracdnc <bugracaliskandinc@gmail.com>
2024-10-09 23:04:10 +0300

    Prettifier

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import TextIcon from './components/TextIcon.vue';
-import Info from './components/PersonalInfo.vue';
-import Commands from './components/CmdLines.vue';
+import TextIcon from './components/TextIcon.vue'
+import Info from './components/PersonalInfo.vue'
+import Commands from './components/CmdLines.vue'
 </script>
 
 <template>
--- src/components/CmdLines.vue
+++ src/components/CmdLines.vue
@@ -1,42 +1,62 @@
 <script setup lang="ts">
-
 interface Command {
-    comment: string;
-    command: string;
-    link: string;
-    text: string;
+  comment: string
+  command: string
+  link: string
+  text: string
 }
 
-var commands: Command[] = [];
-commands.push({ comment: "source code", command: "curl", link: "https://github.com/bugracdnc/grabu.dev", text: "github.com/bugracdnc/grabu.dev" });
-commands.push({ comment: "main website", command: "curl", link: "https://grabu.dev", text: "grabu.dev" });
-commands.push({ comment: "my cv in english", command: "curl", link: "https://grabu.dev/cv/en", text: "grabu.dev/cv/en" });
-commands.push({ comment: "türkçe özgeçmişim", command: "curl", link: "https://grabu.dev/cv/tr", text: "grabu.dev/cv/tr" });
+var commands: Command[] = []
+commands.push({
+  comment: 'source code',
+  command: 'curl',
+  link: 'https://github.com/bugracdnc/grabu.dev',
+  text: 'github.com/bugracdnc/grabu.dev'
+})
+commands.push({
+  comment: 'main website',
+  command: 'curl',
+  link: 'https://grabu.dev',
+  text: 'grabu.dev'
+})
+commands.push({
+  comment: 'my cv in english',
+  command: 'curl',
+  link: 'cv/en',
+  text: 'grabu.dev/cv/en'
+})
+commands.push({
+  comment: 'türkçe özgeçmişim',
+  command: 'curl',
+  link: 'cv/tr',
+  text: 'grabu.dev/cv/tr'
+})
 </script>
 
 <template>
-    <div v-for="command in commands" :key="command.command">
-        <span class="comment"># {{ command.comment }}</span>
-        <span class="PS1">$</span>
-        {{ command.command }}
-        <a v-bind:href="command.link">{{ command.text }}</a><br /><br />
-    </div>
+  <div v-for="command in commands" :key="command.command">
+    <span class="comment"># {{ command.comment }}</span>
+    <span class="PS1">$</span>
+    {{ command.command }}
+    <a v-bind:href="command.link">{{ command.text }}</a
+    ><br /><br />
+  </div>
 </template>
 
 <style lang="css" scoped>
 a {
-    margin-bottom: 1em;
+  margin-bottom: 1em;
 }
 
 .comment {
-    display: block;
+  display: block;
 }
 
 .PS1 {
-    color: #1A1;
+  color: #1a1;
 }
 
 .comment {
-    color: #444
+  color: #444;
 }
 </style>
