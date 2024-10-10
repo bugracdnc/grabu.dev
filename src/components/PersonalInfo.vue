5735f4f6329b6a638077622b523108e49a461474
bugracdnc <bugracaliskandinc@gmail.com>
2024-10-10 10:57:28 +0300

    Update PersonalInfo with LinkedIn, and formatting

--- src/components/PersonalInfo.vue
+++ src/components/PersonalInfo.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="info_entry">grabu - Buğra Çalışkandinç</div>
+  <span class="comment"># me</span>
+  <div class="info_entry">Buğra Çalışkandinç</div><br />
+  <span class="comment"># my links</span>
   <div class="info_entry">
-    <a href="https://github.com/bugracdnc">https://github.com/bugracdnc</a>
+    <a href="https://github.com/bugracdnc">github.com/bugracdnc</a>
+  </div>
+  <div class="info_entry">
+    <a href="https://www.linkedin.com/in/bugracdnc/">linkedin.com/in/bugracdnc</a>
   </div>
 </template>
